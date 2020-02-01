88bdd3ffcbab2855749c25afdcb20e93e7070781
LoboAnimae <30876166+LoboAnimae@users.noreply.github.com>
2020-02-01 12:49:37 -0600

    Push

---
+++ Proyect/Assets/Scripts/player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float maxSpeed = 5f;
+    public float acceleration = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    rb = GetComponent<Rigidbody2D>();
+    rb.velocity = new Vector2(0.0f, 0.0f);  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(rb.velocity.magnitude>maxSpeed){
+            rb.velocity=rb.velocity.normalized*maxSpeed;
+        }
+       
+        float horizontal = Input.GetAxis("Horizontal") * acceleration * Time.deltaTime;
+        float vertical = Input.GetAxis("Vertical") * acceleration * Time.deltaTime;
+
+        rb.AddForce(new Vector2(horizontal, vertical), ForceMode2D.Impulse);
+        
+    }
+}
