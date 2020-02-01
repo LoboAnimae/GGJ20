68f3b34fc5d098efd61c1016bff7bbfe31f17d21
LoboAnimae <30876166+LoboAnimae@users.noreply.github.com>
2020-02-01 11:24:21 -0600

    2

---
+++ Proyect/Assets/Scripts/Init_Vel_Met.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Init_Vel_Met : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rb = GetComponent<Rigidbody2D>();
+        //rb.velocity = new Vector2(Random.Range(-2.0f, 2.0f), Random.Range(-2.0f, 2.0f));
+        rb.rotation = 45.0f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.rotation = rb.rotation + 0.01f;
+        Debug.Log(rb.rotation + "current value");
+    }
+}
