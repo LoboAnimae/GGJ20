cf2a17f37a930e1b3e05353d22e3d6718792cac4
Mec4no <35870265+Mec4no@users.noreply.github.com>
2020-02-01 21:19:42 -0600

    Added animations

---
+++ Proyect/Assets/Scripts/camera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    public player user;
+    public bool isFollowing;
+    public float wOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     user = FindObjectOfType<player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = new Vector3(user.transform.position.x, user.transform.position.y, -10f);
+    }
+}
--- Proyect/Assets/Scripts/player.cs
+++ Proyect/Assets/Scripts/player.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class player : MonoBehaviour
+public class player  : MonoBehaviour
 {
+    public Animator animator;  
     public Rigidbody2D rb;
     public float maxSpeed = 5f;
     public float acceleration = 10f;
+    public int direction = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -18,13 +20,24 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)){
+            Debug.Log("D or Right Arrow Pressed");
+            direction = 1;
+            animator.SetInteger("Direction", direction);
+
+        }
+        if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)){
+            Debug.Log("A or Left arrow pressed");
+            direction = 2;
+            animator.SetInteger("Direction", direction);
+        }
         if(rb.velocity.magnitude>maxSpeed){
             rb.velocity=rb.velocity.normalized*maxSpeed;
         }
        
         float horizontal = Input.GetAxis("Horizontal") * acceleration * Time.deltaTime;
         float vertical = Input.GetAxis("Vertical") * acceleration * Time.deltaTime;
-
+        
         rb.AddForce(new Vector2(horizontal, vertical), ForceMode2D.Impulse);
         
     }
