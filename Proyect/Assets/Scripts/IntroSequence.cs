fbafb88e422bcbfaca26bda93c9cb6e94121d413
Mec4no <35870265+Mec4no@users.noreply.github.com>
2020-02-02 12:46:30 -0600

    ok

---
+++ Proyect/Assets/Scripts/IntroSequence.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class IntroSequence : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject titleAnim;
+    private Animator anim;
+    private bool flag;
+    // Start is called before the first frame update
+    void Start()
+    {
+      anim = titleAnim.GetComponent<Animator>();
+      //animation = anim.
+      //anim.Play();
+      flag =  false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1) {
+          SceneManager.LoadScene("MainMenu");
+        }
+      /*if(flag == true) {
+
+      }*/
+    }
+}
