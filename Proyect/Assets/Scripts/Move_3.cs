6996d0bb661301854d19081b4228f26feae734f4
Arem58 <46797233+Arem58@users.noreply.github.com>
2020-02-01 19:06:58 -0600

    Add files via upload

---
+++ Proyect/Assets/Scripts/Move_3.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_3 : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public int simbolo;
+    public int simbolo2;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        if (Random.Range(-1f, 1f) > 0 || Random.Range(-1f, 1f) == 0)
+        {
+            simbolo = 1;
+        }
+        else
+        {
+            simbolo = -1;
+        }
+
+        if (Random.Range(-1f, 1f) > 0 || Random.Range(-1f, 1f) == 0)
+        {
+            simbolo2 = 1;
+        }
+        else
+        {
+            simbolo2 = -1;
+        }
+
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(simbolo * 25, simbolo2 * 25);
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+
+    }
+}
